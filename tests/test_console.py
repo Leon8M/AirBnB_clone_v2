d12aad241c429f61793f4ed42a21954925549a30
Leon8M <leonmunene254@gmail.com>
2023-09-18 15:54:28 +0300

    weeb

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,28 +17,6 @@ class TestHBNBCommand(unittest.TestCase):
     def tearDown(self):
         del self.console
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'FileStorage test')
-    def test_create_db(self):
-        """Test create command with database storage."""
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("create User email='test@example.com' password='123'")
-            output = mock_stdout.getvalue().strip()
-            self.assertTrue(re.match(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', output))
-            self.assertIn('User', output)
-            self.assertTrue(storage.get("User", output))
-
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'FileStorage test')
-    def test_show_db(self):
-        """Test show command with database storage."""
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("create User email='test@example.com' password='123'")
-            obj_id = mock_stdout.getvalue().strip()
-            self.console.onecmd(f"show User {obj_id}")
-            output = mock_stdout.getvalue().strip()
-            self.assertIn(obj_id, output)
-            self.assertIn("test@example.com", output)
-            self.assertIn("123", output)
-
     def test_create_file(self):
         """Test create command with file storage."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
