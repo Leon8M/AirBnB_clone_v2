ff2273e591a7fcf7e265ed0bf73e851c0dc23318
Leon8M <leonmunene254@gmail.com>
2023-09-18 14:24:56 +0300

    8

--- models/user.py
+++ models/user.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""This module defines a class User"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
