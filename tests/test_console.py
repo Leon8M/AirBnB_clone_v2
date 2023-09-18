1aa426f9c3551f94d4df65b7637c9783d6a13e92
Leon8M <leonmunene254@gmail.com>
2023-09-18 15:52:42 +0300

    !02

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,136 +1,77 @@
-#!/usr/bin/python3
-"""A unit test module for the console (command interpreter).
-"""
-import os
 import unittest
-from io import StringIO
 from unittest.mock import patch
-import MySQLdb
-import sqlalchemy
+from io import StringIO
+import os
+import sys
+import re
 
 from console import HBNBCommand
+from models.base_model import BaseModel
 from models import storage
-from models.user import User
-from tests import clear_stream
 
 
 class TestHBNBCommand(unittest.TestCase):
-    """Represents the test class for the HBNBCommand class.
-    """
-    @unittest.skipIf(
-        os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
-    def test_fs_create(self):
-        """Tests the create command with the file storage.
-        """
-        with patch('sys.stdout', new=StringIO()) as cout:
-            cons = HBNBCommand()
-            cons.onecmd('create City name="Texas"')
-            mdl_id = cout.getvalue().strip()
-            clear_stream(cout)
-            self.assertIn('City.{}'.format(mdl_id), storage.all().keys())
-            cons.onecmd('show City {}'.format(mdl_id))
-            self.assertIn("'name': 'Texas'", cout.getvalue().strip())
-            clear_stream(cout)
-            cons.onecmd('create User name="James" age=17 height=5.9')
-            mdl_id = cout.getvalue().strip()
-            self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
-            clear_stream(cout)
-            cons.onecmd('show User {}'.format(mdl_id))
-            self.assertIn("'name': 'James'", cout.getvalue().strip())
-            self.assertIn("'age': 17", cout.getvalue().strip())
-            self.assertIn("'height': 5.9", cout.getvalue().strip())
+    def setUp(self):
+        self.console = HBNBCommand()
 
-    @unittest.skipIf(
-        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-    def test_db_create(self):
-        """Tests the create command with the database storage.
-        """
-        with patch('sys.stdout', new=StringIO()) as cout:
-            cons = HBNBCommand()
-            clear_stream(cout)
-            cons.onecmd('create User email="john25@gmail.com" password="123"')
-            mdl_id = cout.getvalue().strip()
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(mdl_id))
-            result = cursor.fetchone()
-            self.assertTrue(result is not None)
-            self.assertIn('john25@gmail.com', result)
-            self.assertIn('123', result)
-            cursor.close()
-            dbc.close()
+    def tearDown(self):
+        del self.console
 
-    @unittest.skipIf(
-        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-    def test_db_show(self):
-        """Tests the show command with the database storage.
-        """
-        with patch('sys.stdout', new=StringIO()) as cout:
-            cons = HBNBCommand()
-            obj = User(email="john25@gmail.com", password="123")
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(obj.id))
-            result = cursor.fetchone()
-            self.assertTrue(result is None)
-            cons.onecmd('show User {}'.format(obj.id))
-            self.assertEqual(
-                cout.getvalue().strip(),
-                '** no instance found **'
-            )
-            obj.save()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(obj.id))
-            clear_stream(cout)
-            cons.onecmd('show User {}'.format(obj.id))
-            result = cursor.fetchone()
-            self.assertTrue(result is not None)
-            self.assertIn('john25@gmail.com', result)
-            self.assertIn('123', result)
-            self.assertIn('john25@gmail.com', cout.getvalue())
-            self.assertIn('123', cout.getvalue())
-            cursor.close()
-            dbc.close()
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'FileStorage test')
+    def test_create_db(self):
+        """Test create command with database storage."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("create User email='test@example.com' password='123'")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(re.match(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', output))
+            self.assertIn('User', output)
+            self.assertTrue(storage.get("User", output))
 
-    @unittest.skipIf(
-        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-    def test_db_count(self):
-        """Tests the count command with the database storage.
-        """
-        with patch('sys.stdout', new=StringIO()) as cout:
-            cons = HBNBCommand()
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT COUNT(*) FROM states;')
-            res = cursor.fetchone()
-            prev_count = int(res[0])
-            cons.onecmd('create State name="Enugu"')
-            clear_stream(cout)
-            cons.onecmd('count State')
-            cnt = cout.getvalue().strip()
-            self.assertEqual(int(cnt), prev_count + 1)
-            clear_stream(cout)
-            cons.onecmd('count State')
-            cursor.close()
-            dbc.close()
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'FileStorage test')
+    def test_show_db(self):
+        """Test show command with database storage."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("create User email='test@example.com' password='123'")
+            obj_id = mock_stdout.getvalue().strip()
+            self.console.onecmd(f"show User {obj_id}")
+            output = mock_stdout.getvalue().strip()
+            self.assertIn(obj_id, output)
+            self.assertIn("test@example.com", output)
+            self.assertIn("123", output)
 
+    def test_create_file(self):
+        """Test create command with file storage."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("create User email='test@example.com' password='123'")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(re.match(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', output))
+            self.assertIn('User', output)
+            self.assertTrue(storage.get("User", output))
+
+    def test_show_file(self):
+        """Test show command with file storage."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("create User email='test@example.com' password='123'")
+            obj_id = mock_stdout.getvalue().strip()
+            self.console.onecmd(f"show User {obj_id}")
+            output = mock_stdout.getvalue().strip()
+            self.assertIn(obj_id, output)
+            self.assertIn("test@example.com", output)
+            self.assertIn("123", output)
+
+    def test_quit(self):
+        """Test the quit command."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("quit")
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_EOF(self):
+        """Test the EOF command."""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.console.onecmd("EOF")
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, "")
 
 if __name__ == '__main__':
     unittest.main()
