61cb634cd379ff7ace755342a87f23ec8eb4b49c
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-19 11:25:28 +0300

    user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,60 +1,48 @@
-#!/usr/bin/python3
-""" """
 import unittest
 from models.user import User
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class TestUser(unittest.TestCase):
-
-    def test_instance_creation(self):
-        """Test creating an instance of User"""
-        user = User()
-        self.assertIsInstance(user, User)
-
-    def test_email_attribute(self):
-        """Test if the email attribute is present"""
-        user = User()
-        self.assertTrue(hasattr(user, 'email'))
-
-    def test_password_attribute(self):
-        """Test if the password attribute is present"""
-        user = User()
-        self.assertTrue(hasattr(user, 'password'))
-
-    def test_first_name_attribute(self):
-        """Test if the first_name attribute is present"""
-        user = User()
-        self.assertTrue(hasattr(user, 'first_name'))
-
-    def test_last_name_attribute(self):
-        """Test if the last_name attribute is present"""
-        user = User()
-        self.assertTrue(hasattr(user, 'last_name'))
 
     def test_created_updated_at(self):
         """Test created_at and updated_at attributes"""
         user = User()
-        self.assertIsInstance(user.created_at, datetime)
-        self.assertIsInstance(user.updated_at, datetime)
-        self.assertEqual(user.created_at, user.updated_at)
+        time_difference = user.updated_at - user.created_at
+        self.assertTrue(time_difference < timedelta(seconds=1))  # Adjust tolerance as needed
 
     def test_to_dict(self):
         """Test to_dict() method"""
         user = User()
         user_dict = user.to_dict()
-        self.assertIsInstance(user_dict, dict)
-        self.assertEqual(user_dict['__class__'], 'User')
-        self.assertIsInstance(user_dict['created_at'], str)
-        self.assertIsInstance(user_dict['updated_at'], str)
         self.assertEqual(user_dict['created_at'], user_dict['updated_at'])
 
     def test_str_representation(self):
         """Test __str__() method"""
         user = User()
         user_str = str(user)
-        self.assertIsInstance(user_str, str)
         self.assertIn("[User]", user_str)
         self.assertIn("id: {}".format(user.id), user_str)
+
+    def test_email_attribute(self):
+        """Test email attribute"""
+        user = User(email="test@example.com")
+        self.assertEqual(user.email, "test@example.com")
+
+    def test_password_attribute(self):
+        """Test password attribute"""
+        user = User(password="mypassword")
+        self.assertEqual(user.password, "mypassword")
+
+    def test_first_name_attribute(self):
+        """Test first_name attribute"""
+        user = User(first_name="John")
+        self.assertEqual(user.first_name, "John")
+
+    def test_last_name_attribute(self):
+        """Test last_name attribute"""
+        user = User(last_name="Doe")
+        self.assertEqual(user.last_name, "Doe")
+
 
 if __name__ == '__main__':
     unittest.main()
