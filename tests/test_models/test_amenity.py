588fb819923b2a1831a5ae3e377073a378969f7e
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-19 12:05:17 +0300

    tesnting amenities

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,40 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
 from models.amenity import Amenity
 
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        """Set up a test instance of Amenity before each test."""
+        self.amenity = Amenity()
 
-class test_Amenity(test_basemodel):
-    """ """
+    def tearDown(self):
+        """Clean up after each test."""
+        del self.amenity
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    def test_amenity_instance(self):
+        """Test if an instance of Amenity is created successfully."""
+        self.assertIsInstance(self.amenity, Amenity)
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_amenity_attributes(self):
+        """Test Amenity attributes."""
+        self.assertTrue(hasattr(self.amenity, 'name'))
+        self.assertEqual(self.amenity.name, "")
+
+    def test_to_dict_method(self):
+        """Test the to_dict() method of Amenity."""
+        amenity_dict = self.amenity.to_dict()
+        self.assertIsInstance(amenity_dict, dict)
+        self.assertIn('id', amenity_dict)
+        self.assertIn('created_at', amenity_dict)
+        self.assertIn('updated_at', amenity_dict)
+        self.assertIn('name', amenity_dict)
+        self.assertEqual(amenity_dict['__class__'], 'Amenity')
+
+    def test_str_representation(self):
+        """Test the __str__() method of Amenity."""
+        amenity_str = str(self.amenity)
+        self.assertIsInstance(amenity_str, str)
+        self.assertIn("[Amenity]", amenity_str)
+        self.assertIn(str(self.amenity.id), amenity_str)
+
+if __name__ == '__main__':
+    unittest.main()
