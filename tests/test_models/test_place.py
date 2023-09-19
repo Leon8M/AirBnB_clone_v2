e24af01b081c592e070262beacd4c9ac5fe2aa9a
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-19 12:27:20 +0300

    longitude

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -16,47 +16,51 @@ class test_Place(test_basemodel):
     def test_city_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.city_id), None)
+        self.assertIsNone(new.longitude)
+
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), None)
+        self.assertIsNone(new.longitude)
 
     def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), None)
+        self.assertIsNone(new.longitude)
+
 
     def test_description(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.description), None)
+        self.assertIsNone(new.longitude)
 
     def test_number_rooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_rooms), None)
+        self.assertIsNone(new.longitude)
+
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), None)
+        self.assertIsNone(new.longitude)
+
 
     def test_max_guest(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.max_guest), None)
+        self.assertIsNone(new.longitude)
 
     def test_price_by_night(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.price_by_night), None)
+        self.assertIsNone(new.longitude)
 
     def test_latitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), None)
+        self.assertEqual(new.latitude, None)
 
     def test_longitude(self):
         """ """
