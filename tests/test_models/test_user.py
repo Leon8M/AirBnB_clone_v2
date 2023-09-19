239b2d330d4242e6398f61e7c09f77faa4872ad1
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-19 11:17:30 +0300

    User.py test_file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,60 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
 from models.user import User
+from datetime import datetime
 
+class TestUser(unittest.TestCase):
 
-class test_User(test_basemodel):
-    """ """
+    def test_instance_creation(self):
+        """Test creating an instance of User"""
+        user = User()
+        self.assertIsInstance(user, User)
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    def test_email_attribute(self):
+        """Test if the email attribute is present"""
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def test_password_attribute(self):
+        """Test if the password attribute is present"""
+        user = User()
+        self.assertTrue(hasattr(user, 'password'))
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+    def test_first_name_attribute(self):
+        """Test if the first_name attribute is present"""
+        user = User()
+        self.assertTrue(hasattr(user, 'first_name'))
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+    def test_last_name_attribute(self):
+        """Test if the last_name attribute is present"""
+        user = User()
+        self.assertTrue(hasattr(user, 'last_name'))
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+    def test_created_updated_at(self):
+        """Test created_at and updated_at attributes"""
+        user = User()
+        self.assertIsInstance(user.created_at, datetime)
+        self.assertIsInstance(user.updated_at, datetime)
+        self.assertEqual(user.created_at, user.updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict() method"""
+        user = User()
+        user_dict = user.to_dict()
+        self.assertIsInstance(user_dict, dict)
+        self.assertEqual(user_dict['__class__'], 'User')
+        self.assertIsInstance(user_dict['created_at'], str)
+        self.assertIsInstance(user_dict['updated_at'], str)
+        self.assertEqual(user_dict['created_at'], user_dict['updated_at'])
+
+    def test_str_representation(self):
+        """Test __str__() method"""
+        user = User()
+        user_str = str(user)
+        self.assertIsInstance(user_str, str)
+        self.assertIn("[User]", user_str)
+        self.assertIn("id: {}".format(user.id), user_str)
+
+if __name__ == '__main__':
+    unittest.main()
