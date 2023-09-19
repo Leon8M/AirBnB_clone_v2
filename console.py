480352abc557ff7ad151903a91715d5d42d03195
Leon8M <leonmunene254@gmail.com>
2023-09-19 11:09:41 +0300

    test9

--- console.py
+++ console.py
@@ -96,7 +96,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
-        exit()
+        raise SystemExit
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
@@ -105,7 +105,7 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
         print()
-        exit()
+        raise SystemExit
 
     def help_EOF(self):
         """ Prints the help documentation for EOF """
